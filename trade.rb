d9f7998e94bfb2bca49921b73a17a5328696749f
sunaina <sushrivastava@bestpeers.com>
2018-04-20 14:55:43 +0530

    edited the calling file

--- trade.rb
+++ trade.rb
@@ -11,8 +11,9 @@ load 'sellers.rb'
 load 'productdatasource.rb'
 
 puts 'User details are -'
-UserDataSource.import_user_data
+Buyers.import_user_data
 Buyers.all.each { |buyer| puts buyer.details }
+Sellers.import_user_data
 Sellers.all.each { |seller| puts seller.details }
 puts
 
@@ -27,37 +28,41 @@ Prabhu.import_product_data
 Prabhu.all.each { |seller| puts seller.details + "\n\n" }
 
 puts 'Buyers age are -'
-p Buyers.all
 Buyers.all.each { |buyer| puts buyer.age }
 puts
 
 puts 'Buyers full_name are -'
+Buyers.import_user_data
 Buyers.all.each { |buyer| puts buyer.full_name }
 puts
 
 puts 'Buyers details are -'
+Buyers.import_user_data
 Buyers.all.each { |buyer| puts buyer.details }
 puts
 
 puts 'Sellers age are -'
+Sellers.import_user_data
 Sellers.all.each { |seller| puts seller.age }
 puts
 
 puts 'Sellers full_name are -'
+Sellers.import_user_data
 Sellers.all.each { |seller| puts seller.full_name }
 puts
 
 puts 'Sellers details are -'
+Sellers.import_user_data
 Sellers.all.each { |seller| puts seller.details }
 puts
 
 puts 'Users having name "alex" are -'
-
+Buyers.import_user_data
 Buyers.all.each do |buyer|
   @buyers = buyer.details if buyer.first_name == 'alex'
 end
 puts @buyers
-
+Sellers.import_user_data
 Sellers.all.each do |seller|
   @sellers = seller.details if seller.first_name == 'alex'
 end
